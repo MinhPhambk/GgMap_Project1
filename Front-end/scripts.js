09b03e4f676faa2d80628e6bf18416c764086215
VuHung <133280335+hungnv150203@users.noreply.github.com>
2023-12-07 22:07:58 +0700

    Change Front-end (#2)

--- Front-end/scripts.js
+++ Front-end/scripts.js
@@ -1,13 +1,13 @@
 // Initialize the Leaflet map
-var hoanKiemBounds = L.latLngBounds([21.0208, 105.8422], [21.0338, 105.8581]);
+var nguyenTrungTruc = L.latLngBounds([21.045001, 105.846156], [21.039919, 105.847561]);
 
 // Initialize the map with custom settings
 var map = L.map('map', {
-  maxBounds: hoanKiemBounds,
+  maxBounds: nguyenTrungTruc,
   maxBoundsViscosity: 1.0,
-  center: [21.0278, 105.8497],
-  zoom: 16,
-  minZoom: 16
+  center: [21.041947, 105.847218],
+  zoom: 17,
+  minZoom: 13,
 });
 
 // Add a base map layer
@@ -15,11 +15,21 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// Add a marker for Hoan Kiem Ward, Hanoi
-L.marker([21.0285, 105.8501]).addTo(map)
-  .bindPopup("Hoan Kiem Ward, Hanoi").openPopup();
+// Add a marker for P.Nguyen Trung Truc, Hanoi
+L.marker([21.041947, 105.847218]).addTo(map)
+  .bindPopup("P.Nguyen Trung Truc, Hanoi").openPopup();
 
-// Add event listener for map click to update input values
+// Add a popup show coordinates when click on map
+var popup = L.popup();
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+map.on('click', onMapClick);
+
+  // Add event listener for map click to update input values
 map.on('click', function (e) {
   var startInput = document.getElementById('start-input');
   var destinationInput = document.getElementById('destination-input');
@@ -30,3 +40,35 @@ map.on('click', function (e) {
     destinationInput.value = e.latlng.lat + ', ' + e.latlng.lng;
   }
 });
+
+// Create a border around the area
+function initMap() {
+  var polylinePoints = [
+    [21.044948, 105.846164],
+    [21.043853, 105.845809],
+    [21.043825, 105.845518],
+    [21.042426, 105.845311],
+    [21.042141, 105.845083],
+    [21.041926, 105.844719],
+    [21.041564, 105.844602],
+    [21.041345, 105.844507],
+    [21.041151, 105.844735],
+    [21.041255, 105.845052],
+    [21.040525, 105.846136],
+    [21.040474, 105.846558],
+    [21.040689, 105.847101],
+    [21.039978, 105.847558],
+    [21.040668, 105.850000],
+    [21.040863, 105.850244],
+    [21.044948, 105.846164],
+    ];
+
+  var polyline = L.polyline(polylinePoints, { 
+    color: 'red',
+    dashArray: '5, 5',
+    opacity: 0.7
+  }).addTo(map);
+}
+  document.addEventListener('DOMContentLoaded', function() {
+  initMap();
+  });
